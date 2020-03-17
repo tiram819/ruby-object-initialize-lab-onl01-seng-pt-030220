27101df54592df974a0df004dc41ffe1e359d037
Tabassum Hassan <tiram819@gmail.com>
2020-03-17 00:31:32 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,12 @@
 class Dog
-  def initialize (dogs_name, dogs_breed = "Mutt")
+  def initialize (name, breed = "Mutt")
     @name = dogs_name
     @breed = dogs_breed
   end
 
-  def name=(dogs_name, dogs_breed = "Mutt")
-    @name = dogs_name
-    @breed = dogs_breed
+  def name=(name, breed = "Mutt")
+    @name = name
+    @breed = breed
   end
 
   def name
